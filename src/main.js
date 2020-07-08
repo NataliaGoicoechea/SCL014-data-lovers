e5b0ca3976613684f6ba45f33001c327f4e1cad7
Natalia Goicoechea <na.goicoechea@gmail.com>
2020-07-07 23:43:45 -0400

    funcion showcharacters y css

--- src/main.js
+++ src/main.js
@@ -1,44 +1,27 @@
-//llamando a la data.js
-//import { characters } from './data.js';
-
 import data from './data/potter/potter.js';
 import { characters } from './data.js';
 
-//console.log(data);
+//variable que muestra todos los personajes en la página principal
 
-showCharacters();
-function showCharacters(){
-    let characters = [];
+const allCharacters = document.getElementById('root'); //llamando al ID del html dentro d ela variable allcharacters
+function showCharacters(){ 
+    let characters = ''; //declarando variable de tipo string
      for (let i = 0; i < data.length; i++){
-        // Characters = characters + data[i];
-        //console.log(data[i]);    
-        characters.push(data[i]);
-
+     let card = document.createElement('article'); //a la variable card le estamos asignando el elemento article
+     card.setAttribute('class','card'); // le estamos añadiendo un atributo class a article
+     allCharacters.appendChild(card); //card es el hijo de allcharacters
+     characters = //html dinámico, se esta concatenando la data con los elementos html
+     `<p class='' id=''>hola ${data[i].name}</p>
+     <p>Patronus: ${data[i].patronus} </p>
+     <img src='${data[i].image}'>`;
+     card.innerHTML = characters; //estamos imprimiendo en html nuestro card que contiene la variable personajes
     }; 
-    //console.log(characters);
-    return characters;
 };
 
 console.log(showCharacters());
 
-let name = showCharacters().map(function(name){
-   return name.name;
-});
-document.getElementById('root').innerHTML = name;
-
-/*const data = {
-showCharacters = 
- for (let i = 0; i < data.length; i++){
-    let name = '';
-
-    console.log(data[i]);    
-}; 
-}*/
-
-//const characters = ;
-
 // TRAER TODOS LOS PERSONAJES E IMPRIMIRLOS EN HTML
-// TRAER Y GUARDAR EL BOTON (DOCUMENT)
+// TRAER Y GUARDAR EL BOTON (DOCUMENT)  
 // DARLE EVENTO (ADDEVENTLISTENNER)
 // IR A LA DAT Y RECORRERLA (FOR)
 // IMPRIMIR NOMBRES EN CONSOLE.LOG
