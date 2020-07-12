06425d913acc6f8be2d7810dca050bdad9fa81c8
Natalia Goicoechea <na.goicoechea@gmail.com>
2020-07-11 22:23:39 -0400

    add images to shield house and remove data to lol, atletas and pokemon, also changes in css

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 import data from './data/potter/potter.js';
-// import { characters } from './data.js';
+import { filterHouseData } from './data.js';
 
-// Agregando un evento al botón entrar
+// Agregando un evento al botón entrar de la pantalla de bienvenida
 document.getElementById('buttonIn').addEventListener('click', showPrincipalScreen);
 
-// Ocultar y mostrar pantallas
+// Ocultar y mostrar pantallas e imprimir nombre de usuario en pantalla principal
 function showPrincipalScreen() {
   const user = document.getElementById('name').value;
   document.getElementById('showName').innerHTML = user;
@@ -14,23 +14,18 @@ function showPrincipalScreen() {
   document.getElementById('footer').classList.remove('not-visible');
 }
 
-/* Buscador
-const find = '';
-document.getElementById('search').onclick = function buscador() {
-  location.href = document.getElementById('search-container').value;
-  document.getElementById('search').innerHTML = find;
-}; */
-
-// Variable que muestra todos los personajes en la página principal
-const allCharacters = document.getElementById('grid'); // llamando al ID del html dentro d ela variable allcharacters
+// Variable que va a buscar toda la data de los personajes y los imprime en la página principal
+const allCharacters = document.getElementById('grid'); // llamando al ID grid dentro de la variable allcharacters
 function showCharacters() {
   let characters = ''; // declarando variable de tipo string
-  for (let i = 0; i < data.length; i++) {
+  for (let i = 0; i < data.length; i++) { // Ir a la data y recorrerla
     const card = document.createElement('article'); // a la variable card le estamos asignando el elemento article
     allCharacters.appendChild(card); // card es el hijo de allcharacters
     card.setAttribute('class', 'card'); // le estamos añadiendo un atributo class a article ,luego html dinámico, se esta concatenando la data con los elementos html
     characters = 
-      `<div style='display:flex;margin:0px;width:100%'>
+      `<div class='border'>
+      <img class='shield' src='${data[i].shield}'>
+        <div style='display:flex;margin:0px;width:100%'>
         <div style='width:50%;height:100%;text-align:center'>
           <h3 id='nameCharacter'>${data[i].name} <hr></h3>
           <div class='box-icon'>
@@ -54,8 +49,19 @@ function showCharacters() {
         <div id='img-card'>
         <img class='img-card' src='${data[i].image}'>
         </div>
+        </div>
       </div>`;
+      card.innerHTML = characters;
+    }
+  }
+  showCharacters();
 
+// Filtro Personajes Casa de Estudio
+//const houseCharacters = data.map(function filterHouse(house,[i]))
+//filterHouseData();
+
+
+ // Futuro Modal que muestra al personaje a fondo.
     /*    `<div style='display:flex;margin:0px;width:100%'>
         <div style='width:50%;height:100%;text-align:center'>
         <p class='' id='nameCharacter'>${data[i].name}</p>
@@ -66,13 +72,11 @@ function showCharacters() {
         <img class='img-card' src='${data[i].image}'>
         </div>
         </div>`;*/
-        card.innerHTML = characters;
-        // card.appendChild(characters); // estamos imprimiendo en html nuestro card que contiene la variable personajes
-  }
-}
-showCharacters();
-// TRAER TODOS LOS PERSONAJES E IMPRIMIRLOS EN HTML
-// TRAER Y GUARDAR EL BOTON (DOCUMENT)  
-// DARLE EVENTO (ADDEVENTLISTENNER)
-// IR A LA DAT Y RECORRERLA (FOR)
-// IMPRIMIR NOMBRES EN CONSOLE.LOG
+       // card.appendChild(characters); // estamos imprimiendo en html nuestro card que contiene la variable personajes
+
+/*  Futuro Buscador
+const find = '';
+document.getElementById('search').onclick = function buscador() {
+  location.href = document.getElementById('search-container').value;
+  document.getElementById('search').innerHTML = find;
+}; */
